5c4fd4367d14140a22a179791410fde35af197f3
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-12 17:44:15 +0200

    updated js file

---
+++ app.js
@@ -0,0 +1,6 @@
+//search function
+let userInput = document.getElementById("input");
+let submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("submit", () => {
+  let username = userInput.value;
+});

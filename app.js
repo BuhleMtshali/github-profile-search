1ccdb97a7d42c72c2d66c178577d8efc671afc57
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-12 17:56:15 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 //search function
-let userInput = document.getElementById("input");
-let submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("submit", () => {
-  let username = userInput.value;
-});
+const searchBtn = document.getElementById("submitBtn");
+searchBtn.addEventListener("submit", searchProfile);
+
+//profile for input value which will call the api function
+function searchProfile(event) {
+  event.preventDefault();
+  let inputValue = document.getElementById("input").value;
+  findProfile(inputValue);
+}
+
+//function for api fetch call
+function findProfile(profile) {
+  let apiUrl = `https://api.github.com/users/${profile}`;
+  axios.get(apiUrl).then(renderProfile);
+}
+
+//render function
+function renderProfile(response) {}
